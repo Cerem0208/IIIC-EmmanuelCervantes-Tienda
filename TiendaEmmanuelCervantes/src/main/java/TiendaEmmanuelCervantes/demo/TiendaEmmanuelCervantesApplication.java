fd41f33a6848dab651c075cb940c33ae5c1b39d3
Cerem0208 <lcervantes70209@ufide.ac.cr>
2023-09-20 20:56:28 -0600

    semana 2
    semana 2

---
+++ TiendaEmmanuelCervantes/src/main/java/TiendaEmmanuelCervantes/demo/TiendaEmmanuelCervantesApplication.java
@@ -0,0 +1,13 @@
+package TiendaEmmanuelCervantes.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEmmanuelCervantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEmmanuelCervantesApplication.class, args);
+	}
+
+}
