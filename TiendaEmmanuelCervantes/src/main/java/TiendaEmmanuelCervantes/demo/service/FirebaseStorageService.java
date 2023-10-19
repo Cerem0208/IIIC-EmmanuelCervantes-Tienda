4dee344ff749b5b59f3c0e6e3c788c1a53fc44a8
Cerem0208 <lcervantes70209@ufide.ac.cr>
2023-10-18 19:21:22 -0600

    semana 6
    primera parte de la clase

---
+++ TiendaEmmanuelCervantes/src/main/java/TiendaEmmanuelCervantes/demo/service/FirebaseStorageService.java
@@ -0,0 +1,16 @@
+package TiendaEmmanuelCervantes.demo.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-v1emmanuel.appspot.com";
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "Techshop";
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-v1emmanuel-firebase-adminsdk-czaxg-f3fdff2948.json";
+}
