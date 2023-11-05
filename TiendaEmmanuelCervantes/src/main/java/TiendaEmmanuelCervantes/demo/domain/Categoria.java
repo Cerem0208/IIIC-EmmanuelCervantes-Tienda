1efe90a0a5399b045cad65491b6941b8f1b6d97b
Cerem0208 <lcervantes70209@ufide.ac.cr>
2023-11-05 01:59:13 -0600

    tarea "prueba"

--- TiendaEmmanuelCervantes/src/main/java/TiendaEmmanuelCervantes/demo/domain/Categoria.java
+++ TiendaEmmanuelCervantes/src/main/java/TiendaEmmanuelCervantes/demo/domain/Categoria.java
@@ -1,29 +1,34 @@
 package TiendaEmmanuelCervantes.demo.domain;
+
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
+
 @Data
 @Entity
-@Table(name="categoria")
+@Table(name = "categoria")
 public class Categoria implements Serializable {
-    
+
     private static final long serialVersionUID = 1L;
-//TABLE MAPPING CATEGORIA
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
+    @Column(name = "id_categoria")
     private Long idCategoria;
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
-//TABLE END
-    
-//CONSTRUCTOR 
+
     public Categoria() {
     }
-//CONSTRUCTOR END
+
+    @OneToMany
+    @JoinColumn(name = "id_categoria", updatable = false)
+    List<Producto> productos;
+
     public Categoria(String categoria, boolean activo) {
         this.descripcion = categoria;
         this.activo = activo;
     }
-}//CLASS END
+}
